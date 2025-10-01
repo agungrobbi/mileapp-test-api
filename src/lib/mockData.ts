a7ce9eea533ed1b2ac2a12b76a7efd4a8b9d037c
Agung Kurnia Robbi <agung.robbi@gmail.com>
2025-10-01 08:08:15 +0700

    update mockdata password

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -4,14 +4,14 @@ export const users: IUser[] = [
   {
     id: 1,
     email: 'admin@mile.app',
-    password: 'admin123',
+    password: 'password',
     name: 'Admin User',
     role: 'admin',
   },
   {
     id: 2,
     email: 'user@mile.app',
-    password: 'user123',
+    password: 'password',
     name: 'Test User',
     role: 'user',
   },
